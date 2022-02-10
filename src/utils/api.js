18f18652bd3e6e6c77429ef316d169444c3f1e6f
Dylan Cahoon <dylancahoon651@gmail.com>
2022-02-09 19:21:36 -0500

    small change in api

--- src/utils/api.js
+++ src/utils/api.js
@@ -48,7 +48,7 @@ async function fetchJson(url, options, onCancel) {
 function populateReviews(signal) {
   return async (movie) => {
     console.log("populate reviews", movie)
-    const url = `${API_BASE_URL}/movies/${movie.movie_id}/reviews`;
+    const url = `${API_BASE_URL}/movies/${movie.id}/reviews`;
     movie.reviews = await fetchJson(url, { headers, signal }, []);
     return movie;
   };
